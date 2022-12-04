206d4cc49b3c9d2d5ec9dc6bc7502181ac922da4
Aliaksandr Yakavets <a.yakavets@godeltech.com>
2022-12-04 17:26:33 +0300

    Learning basic methods

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,11 +8,13 @@ public class Test : MonoBehaviour
 {
     [NonSerialized]
     public int _num = 5;
-    [SerializeField]
-    private string Somestr = "Bla";
-    
+      
     public int [] numbers = new int[3];
     public List<string> words = new List<string>(); 
+    
+    private void Awake() {
+      Debug.Log("Awake");
+    }
     void Start()
     {
       numbers[1] = 2;
@@ -22,6 +24,23 @@ public class Test : MonoBehaviour
 
     void Update() 
     {
-    
+      Debug.Log("Update");
+    }
+
+    private void LateUpdate() 
+    {
+      Debug.Log("LateUpdate");
+    }
+
+    private void FixedUpdate() 
+    {
+      Debug.Log("FixedUpdate");
+    }
+    private void OnDestroy() {
+      Debug.Log("OnDestroy");
+    }
+
+    private void OnEnable() {
+      Debug.Log("OnEnable");
     }
 }
